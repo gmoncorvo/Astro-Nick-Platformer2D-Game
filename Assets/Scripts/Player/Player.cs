0c04eccf40044606cb23cac95db6a40e1ffbdb4b
gmoncorvo <gabriel.moncorvoo@gmail.com>
2025-07-11 17:04:27 -0300

    developed jump on movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,10 +8,17 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidBody;
 
     [Header("Player Stats")]
-    //public Vector2 velocity;
+    public Vector2 friction = new Vector2(-0.1f,0);
     public float speed;
+    public float jumpForce = 2f;
 
     void Update()
+    {
+        HandleJump();
+        HandleMovemnt();
+    }
+
+    private void HandleMovemnt()
     {
         if (Input.GetKey(KeyCode.D))
         {
@@ -24,5 +31,22 @@ public class Player : MonoBehaviour
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
         }
 
+        if (myRigidBody.velocity.x > 0f)
+        {
+            myRigidBody.velocity += friction;
+        }
+        if (myRigidBody.velocity.x < 0f)
+        {
+            myRigidBody.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
+            myRigidBody.velocity = Vector2.up * jumpForce;
+        }
     }
 }
