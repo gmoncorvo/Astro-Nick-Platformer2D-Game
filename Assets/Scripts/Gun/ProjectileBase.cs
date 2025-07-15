46b0e6d1cc79d0cad3efe9f6e9c282c2898b4978
gmoncorvo <gabriel.moncorvoo@gmail.com>
2025-07-15 04:30:48 -0300

    minor updates on shooting mechanics

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -7,8 +7,17 @@ public class ProjectileBase : MonoBehaviour
 {
     public Vector3 direction;
 
+    public float timeToDestroy = 2f;
+
+    public float side = 1;
+
+    private void Awake()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
     private void Update()
     {
-        transform.Translate(direction * Time.deltaTime);
+        transform.Translate(direction * Time.deltaTime * side);
     }
 }
