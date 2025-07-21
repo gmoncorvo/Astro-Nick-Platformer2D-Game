fb4c2d19cbca3a3096680b90ca6380d576102804
gmoncorvo <gabriel.moncorvoo@gmail.com>
2025-07-21 10:06:59 -0300

    Update UI for Coins collected

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,18 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Moncrow.Singleton;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
+
+    private void Start()
+    {
+        Reset();
+    }
 
     private void Reset()
     {
         coins = 0;
+        UpdateUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        uiTextCoins.text = coins.ToString();
     }
 }
